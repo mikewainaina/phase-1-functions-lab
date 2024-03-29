c8483b9dbe6240e955cb68f9131f08be91a64291
Michael <michael.wainaina@student.moringaschool.com>
2024-03-29 23:44:34 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const headquarter = 42
+const block = 264
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(pickupLocation - headquarter)
+}
+distanceFromHqInBlocks()
+
+function distanceFromHqInFeet(pickupLocation){
+    return (Math.abs(pickupLocation-headquarter) * block)
+}
+distanceFromHqInFeet()
+
+function distanceTravelledInFeet(start, destination){
+    return (Math.abs(destination-start) * block)
+}
+distanceTravelledInFeets()
+function calculatesFarePrice(start, destination){
+    const feet = (Math.abs(destination-start) * block)
+    if(feet < 400){
+        return 0
+    }else if(feet < 2000){
+        return 2.56
+    }else if(feet < 2500){
+        return 25
+    }else{
+        return "cannot travel that far"
+    }
+}
